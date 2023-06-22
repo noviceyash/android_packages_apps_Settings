Octavi-Testing-Org/packages_apps_Settings

Settings: Build OctaviLab into Settings
Signed-off-by: Sagarrokade006 <sagarrokade006@gmail.com>
Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -168,6 +168,8 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+import com.octavi.lab.OctaviLab;
+
 public class SettingsGateway {
 
     /**
@@ -323,7 +325,8 @@ public class SettingsGateway {
             NetworkProviderSettings.class.getName(),
             NetworkSelectSettings.class.getName(),
             AlarmsAndRemindersDetails.class.getName(),
-            MediaManagementAppsDetails.class.getName()
+            MediaManagementAppsDetails.class.getName(),
+            OctaviLab.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -364,5 +367,6 @@ public class SettingsGateway {
             Settings.MyDeviceInfoActivity.class.getName(),
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
+            Settings.OctaviLabActivity.class.getName(),
     };
 }

noviceyash/android_packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -179,6 +179,8 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+import com.octavi.lab.OctaviLab;
+
 public class SettingsGateway {
 
     /**
@@ -345,7 +347,8 @@ public class SettingsGateway {
             MobileNetworkSettings.class.getName(),
             AppLocaleDetails.class.getName(),
             TurnScreenOnSettings.class.getName(),
-            TurnScreenOnDetails.class.getName()
+            TurnScreenOnDetails.class.getName(),
+            OctaviLab.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -388,5 +391,6 @@ public class SettingsGateway {
             Settings.MyDeviceInfoActivity.class.getName(),
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
+            Settings.OctaviLabActivity.class.getName(),
     };
 }